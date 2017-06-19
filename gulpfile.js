dc0f56ea95a3ca7de2e0a4c0356d6376caeffe6c
nicollehahn <nicollehahn@gmail.com>
2017-06-18 22:26:42 -0400

    Crushed JSON file

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var gulp = require('gulp');
     gulpif = require('gulp-if');
     uglify = require('gulp-uglify');
     minifyHTML = require('gulp-minify-html');
+    jsonminify = require('gulp-jsonminify');
     concat = require('gulp-concat');
 
 var env,
@@ -74,8 +75,8 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch('builds/development/*html', ['html']);
-  gulp.watch(jsonSources, ['json']);
+  gulp.watch('builds/development/*.html', ['html']);
+  gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('connect', function() {
@@ -86,14 +87,16 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src('builds/development/*html')
+  gulp.src('builds/development/*.html')
   .pipe(gulpif(env === 'production', minifyHTML()))
   .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
   .pipe(connect.reload())
 });
 
 gulp.task('json', function() {
-  gulp.src(jsonSources)
+  gulp.src('builds/development/js/*.json')
+  .pipe(gulpif(env === 'production', jsonminify()))
+  .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
   .pipe(connect.reload())
 });
 
